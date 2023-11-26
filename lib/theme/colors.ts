feb0dee87d47597ee60e0dc518b4acee22a4633e
Kamilo Vasquez <killo.blanco@gmail.com>
2023-11-26 09:50:25 -0500

    🎨 Update color shades in colors.ts

--- lib/theme/colors.ts
+++ lib/theme/colors.ts
@@ -12,7 +12,7 @@ export default function colors (colors: Options['colors']) {
 
       return {
         ...acc,
-        [key]: [0, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 98, 100]
+        [key]: [0, 1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 98, 99, 100]
           .reduce((acc, shade) => ({
             ...acc,
             [shade]: hexFromArgb(palette.tone(shade))
