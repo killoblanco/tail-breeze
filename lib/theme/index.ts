6e6892ab90e803bf4085a8045a9c68c69e8a9319
Kamilo Vasquez <killo.blanco@gmail.com>
2023-11-26 15:41:53 -0500

    💄 Add boxShadow and motion to extendTheme

--- lib/theme/index.ts
+++ lib/theme/index.ts
@@ -1,12 +1,16 @@
 import { Config } from 'tailwindcss/types/config';
 import defaultOptions from '../options';
 import colors from './colors';
+import boxShadow from './shadows';
+import { motion } from './motion';
 
 export default function extendTheme (options = defaultOptions): Partial<Config> {
   return {
     theme: {
       extend: {
-        colors: colors({ ...defaultOptions.colors, ...options.colors })
+        boxShadow: boxShadow(),
+        colors: colors({ ...defaultOptions.colors, ...options.colors }),
+        ...motion
       }
     }
   }
