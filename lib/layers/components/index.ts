2d61e2b2d9e6287292a3bc90e516ce0687e9cd5a
Kamilo Vasquez <killo.blanco@gmail.com>
2023-11-26 19:58:57 -0500

    🎨 Add avatar components

--- lib/layers/components/index.ts
+++ lib/layers/components/index.ts
@@ -1,11 +1,13 @@
 import type { CSSRuleObject, PluginAPI } from 'tailwindcss/types/config';
 import defaultOptions from '../../options';
 import { alertComponents } from './alert';
+import { avatarComponents } from './avatar';
 import { buttonComponents } from './button';
 import { typographyComponents } from './typography';
 
 export const componentsLayers = (options = defaultOptions, api: PluginAPI): CSSRuleObject => ({
   ...alertComponents(options, api),
+  ...avatarComponents(options, api),
   ...buttonComponents(options, api),
   ...typographyComponents(options, api),
 })
