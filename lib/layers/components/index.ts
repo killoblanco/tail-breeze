3c41910bbb7d0ccbcf01e508539a34137a29d337
Kamilo Vasquez <killo.blanco@gmail.com>
2023-11-29 20:25:25 -0500

    ✨ Add card components to componentsLayers

--- lib/layers/components/index.ts
+++ lib/layers/components/index.ts
@@ -4,6 +4,7 @@ import { alertComponents } from './alert';
 import { avatarComponents } from './avatar';
 import { badgeComponents } from './badge';
 import { buttonComponents } from './button';
+import { cardComponents } from './cards';
 import { typographyComponents } from './typography';
 
 export const componentsLayers = (options = defaultOptions, api: PluginAPI): CSSRuleObject => ({
@@ -11,5 +12,6 @@ export const componentsLayers = (options = defaultOptions, api: PluginAPI): CSSR
   ...avatarComponents(options, api),
   ...buttonComponents(options, api),
   ...badgeComponents(options, api),
+  ...cardComponents(options, api),
   ...typographyComponents(options, api),
 })
